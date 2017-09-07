3ad61b93d4830d6d22e9a9dc4a5e895eb8a7fe4d
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-09-07 12:20:28 -0600

    Added println to Controller.

--- src/process/controller/ProcessController.java
+++ src/process/controller/ProcessController.java
@@ -4,5 +4,6 @@ public class ProcessController {
 	public void start() {
 		System.out.println("This is a practice Project.");
 		System.out.println("By practicing I develop muscle memory :D");
+		System.out.println("This is a testing line.");
 	}
 }
