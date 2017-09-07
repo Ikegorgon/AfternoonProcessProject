636cdc697585cfe477ea95710deaa32a87679702
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-09-07 12:18:57 -0600

    New Project and changed .getignore file

---
+++ src/process/controller/ProcessController.java
@@ -0,0 +1,8 @@
+package process.controller;
+
+public class ProcessController {
+	public void start() {
+		System.out.println("This is a practice Project.");
+		System.out.println("By practicing I develop muscle memory :D");
+	}
+}
---
+++ src/process/controller/ProcessRunner.java
@@ -0,0 +1,8 @@
+package process.controller;
+
+public class ProcessRunner {
+	public static void main(String [ ] args) {
+		ProcessController appController = new ProcessController();
+		appController.start();
+	}
+}
