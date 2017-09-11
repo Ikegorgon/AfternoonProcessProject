05679436c0bdc2dbba05a2f015ad2445a9235937
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-09-11 12:15:00 -0600

    Made a PlayDohCircle

--- src/process/controller/ProcessController.java
+++ src/process/controller/ProcessController.java
@@ -1,9 +1,19 @@
 package process.controller;
 
+import process.model.PlayDohCircle;
+
 public class ProcessController {
 	public void start() {
 		System.out.println("This is a practice Project.");
 		System.out.println("By practicing I develop muscle memory :D");
 		System.out.println("This is a testing line.");
+		
+		PlayDohCircle firstCircle = new PlayDohCircle();
+		PlayDohCircle secondCircle;
+		secondCircle = new PlayDohCircle();
+		
+		System.out.println("Look  I mad a circle");
+		System.out.println(firstCircle);
+		System.out.println(secondCircle);
 	}
 }
